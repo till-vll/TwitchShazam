2dec085c68535eee89ac08c6d7dff4da945e221e
till-vll <tillsep@gmail.com>
2022-06-20 19:26:40 +0200

    no pykeyboard module

--- selenium_tw.py
+++ selenium_tw.py
@@ -1,3 +1,4 @@
+from keys import chrome_driver_path
 from pyparsing import Opt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -5,8 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from pyshadow.main import Shadow
-from pykeyboard import PyKeyboard ##https://pypi.org/project/PyUserInput/
 from time import sleep
 from twitch_api import Twitch_Api
 
@@ -22,82 +23,72 @@ class Selenium_tw(Twitch_Api):
             options = Options()
             options.add_extension("twitch_shazam/Shazam.crx")
             options.add_extension("twitch_shazam/Twitch-Adblock.crx")
-            chrome_driver_path = r"C:\Users\skarl\Documents\chromedriver"
             self.driver = webdriver.Chrome(chrome_driver_path,options=options)
+            self.driver.implicitly_wait(10)
             self.wait = WebDriverWait(self.driver, 10)
             
             #setup twitch 
             self.driver.get(f"https://twitch.com/{self.channel_name}")
-            self.main_window = self.driver.window_handles[0]
-            self.extension = self.driver.window_handles[1]
             self.accept_cookies()
             if self.check_mature():
                 self.accept_age()
+            
+            if "twitch.tv" in self.driver.current_url:
+                self.main_window = self.driver.window_handles[0]
+                self.extension = self.driver.window_handles[1]
+            else:
+                self.main_window = self.driver.window_handles[1]
+                self.extension = self.driver.window_handles[0]
             self.set_shortcut()
        
     def accept_cookies(self):
         '''Accepts Cookies'''
-        cookie_butt = self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[1]/div/div/div/div[3]/button')
-        self.wait.until(EC.element_to_be_clickable(cookie_butt))
+        cookie_butt = self.wait.until(EC.element_to_be_clickable(self.driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[1]/div/div/div/div[3]/button')))
         cookie_butt.click()
-    
+
     def accept_age(self):
         '''Accepts 18+ if necessary'''
-        sleep(5)
-        age_butt = self.driver.find_element(By.XPATH,'/html/body/div[1]/div/div[2]/div[1]/main/div[2]/div[3]/div/div/div[2]/div/div[2]/div/div[2]/div/div/div[5]/div/div[3]/button')
-        self.wait.until(EC.element_to_be_clickable(age_butt))
+        age_butt = self.wait.until(EC.element_to_be_clickable(self.driver.find_element(By.XPATH,'/html/body/div[1]/div/div[2]/div[1]/main/div[2]/div[3]/div/div/div[2]/div/div[2]/div/div[2]/div/div/div[5]/div/div[3]/button')))
         age_butt.click()
     
     def set_low_settings(self): #TODO set low settings
         '''Sets stream video settings to minimum'''
         pass
     
+    def perform_shortcut(self):
+        '''Performs the shortcut alt + k'''
+        actions = ActionChains(self.driver)
+        actions.key_down(Keys.ALT).perform()
+        sleep(0.5)
+        actions.key_down("k").perform()
+        sleep(0.5)
+        actions.key_up(Keys.ALT).perform()
+        sleep(0.5)
+        actions.key_up('k').perform()
+    
     def try_shazam(self):
         '''Calls the Shazam extension with the shortcut that has been set and waits 15 seconds.'''
-        k = PyKeyboard()
-        #Open extension
-        k.press_key(k.alt_key)
-        k.press_key("K")
-        sleep(1)
-        k.release_key(k.alt_key)
-        k.release_key("K")
-        
-        #wait 15 seconds to find song
+        actions = ActionChains(self.driver)
+        #actions.click().perform()
+        self.perform_shortcut()
         sleep(15)
-        
-        #closes the extension
-        k.press_key(k.alt_key)
-        k.press_key("K")
-        sleep(1)
-        k.release_key(k.alt_key)
-        k.release_key("K")
+        self.perform_shortcut()
 
-    
     def set_shortcut(self):
         '''Sets the shortcut of the extension so you can access it later'''
         url = "chrome://extensions/shortcuts"
+        sleep(0.5)
         self.driver.switch_to.window(self.extension) #switches to the window of the twitch adblock extension
-        sleep(3)
-        
+        sleep(0.5)
         #opens chrome shortcut page
         self.driver.get(url)
         self.driver.maximize_window()
         shadown = Shadow(self.driver) #https://pypi.org/project/pyshadow/
         
-        #sets the shortcut 
-        edit_butt = shadown.find_element('#edit')
-        sleep(1)
+        #sets the shortcut
+        edit_butt = self.wait.until(EC.element_to_be_clickable(shadown.find_element('#edit')))
         edit_butt.click()
-        sleep(1)
-        k = PyKeyboard()
-        k.press_key(k.alt_key)
-        k.press_key("K")
-        sleep(1)
-        k.release_key(k.alt_key)
-        k.release_key("K")
-        sleep(1)
-        
-        #closes the window and switches back to twitch
+        self.perform_shortcut()
         self.driver.close()
         self.driver.switch_to.window(self.main_window)
     
@@ -132,11 +123,10 @@ class Selenium_tw(Twitch_Api):
 
 
 
-# test = Selenium_tw("noway4u_sir")
-# for i in range(3):
-#     test.try_shazam()
-#     sleep(15)
+test = Selenium_tw("noway4u_sir")
+for i in range(3):
+    test.try_shazam()
+    sleep(15)
 
-# songs = test.get_songs()
 # print(songs)
 # sleep(1000)
